f3fc002b5b44b61a046904d4f6d9897bd8e1db64
HoanDV <57141929+Hoanxyz@users.noreply.github.com>
2024-01-23 21:46:13 +0700

    push code

--- src/app/shared/datas/datas.ts
+++ src/app/shared/datas/datas.ts
@@ -6,27 +6,45 @@ export const EVENTS = [
       'tới chỉ thấy “đồng khởi” “mời hết vòng” “bạn ko nể tôi” ….Chốt được chuyến đi thành công với thời tiết siêu ưng, ' +
       'các bạn siêu yêu và không gian siêu chill 🥰 Team tạm thiếu bạn đã có gđình 🤣 hẹn bạn chuyến sau, nhưng ko biết là bao giờ ' +
       '<br>' +
+      '<br>' +
       '<strong>- văn của Yến</strong>',
-    images: ['assets/images/picnic-2.jpg', 'assets/images/picnic-1.jpg'],
-    thumbnail: 'assets/images/picnic-1.jpg'
+    images: ['assets/images/picnic-1.jpg', 'assets/images/picnic-2.jpg'],
+    thumbnail: 'assets/images/picnic-2.jpg'
   },
   {
     title: 'Đám cưới thế kỷ',
     time: '02/12/2023',
-    content: 'abcxyz',
-    images: ['assets/images/cuoi-yen-1.jfif', 'assets/images/cuoi-yen-2.jfif'],
+    content: 'Tôi nhẹ dạ lỡ tin cô gái ấy Hoàng Yến. Hứa đưa đón t thêm vài năm nữa 😌' +
+      'Chưa kịp quên mà nay vì chạy KPI lại lỡ bỏ tôi theo a kia. ' +
+      'Anw, chúc bạn và anh ấy mãi hạnh phúc ❤️ Từ nay, phải sống thật hạnh phúc, vui vẻ nhé 👌Happy wedding! ' +
+      'Thỉnh thoảng đừng quên trốn chồng đi với t 😝' +
+      '<br>' +
+      '<br>' +
+      '<strong>- văn của Hà</strong>',
+    images: ['assets/images/cuoi-yen-2.jfif', 'assets/images/cuoi-yen-3.jpg', 'assets/images/cuoi-yen-1.jfif'],
     thumbnail: 'assets/images/cuoi-yen-1.jfif'
   },
   {
     title: 'Nàng kiều lỡ bước',
     time: '02/12/2023',
-    content: 'abcxyz',
-    images: ['assets/images/cuoi-yen-1.jfif', 'assets/images/cuoi-yen-2.jfif']
+    content: 'Với tư cách quản trị viên #Teamxam ' +
+      'Chúc b Ngọc S của chúng mình sang trang mới thật hạnh phúc nha! 🥰' +
+      'Mà hơn nữa đừng bơ mình 😌' +
+      '<br>' +
+      '<br>' +
+      '<strong>- văn của Hà</strong>',
+    images: ['assets/images/cuoi-ngoc-1.jpg', 'assets/images/cuoi-ngoc-2.jpg'],
+    thumbnail: 'assets/images/cuoi-ngoc-1.jpg'
   },
   {
     title: 'Chấn động, chàng trai bị cô gái lừa...',
     time: '02/12/2023',
-    content: 'abcxyz',
-    images: ['assets/images/cuoi-yen-1.jfif', 'assets/images/cuoi-yen-2.jfif']
+    content: 'Ngta đi countdown cùng nhau 🥳 thì team tôi lại dành trọn kì nghỉ lễ để Unlock 2024 rồi tâm hự đến đêm ' +
+      'cùng bạn Lê TAnh. Thôi thì chúc 2 bạn mãi hạnh phúc🥰' +
+      '<br>' +
+      '<br>' +
+      '<strong>- văn của Hà</strong>',
+    images: ['assets/images/cuoi-lta-3.jpg','assets/images/cuoi-lta-1.jpg','assets/images/cuoi-lta-2.jpg'],
+    thumbnail: 'assets/images/cuoi-lta-2.jpg'
   }
 ]
